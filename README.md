2ed1c9ba83ca6625b24abba8eb8a8f62b4ddb959
Utsav Popli <utsav_popli@hotmail.com>
2021-06-27 18:20:09 -0700

    Move library, utils to the repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ihelp-lib
-# Try changing user name
+# If you are or would like to use your personal email, please cd into directory and run:
+  git config user.email "Your personal email"
+  git config user.name "Your name"
+
+# iHelp-lib
---
+++ index.js
@@ -0,0 +1,26 @@
+var geoddb = require("./db/geoddb")
+var tableManager = require("./db/config")
+
+
+function InitDefaultConfigGeoTable(TBL_NAME) {
+	let client
+	var config = tableManager(TBL_NAME)
+	client = new geoddb(config)
+
+	if (!(await client.TableExists())) {
+		console.log('Table doesnt exist creating new table')
+		await client.CreateTable()
+	} else {
+		console.log('Table already exist')
+	}
+	return client
+}
+
+InitDefaultConfigGeoTable(TST1)
+module.exports = {
+	geoddb: require('./db/geoddb'),
+	gapi: require('./gapi/gapi')
+}
+module.exports.geoddb = this.geoddb
+module.exports.gapi = this.gapi
+console.log(module)
